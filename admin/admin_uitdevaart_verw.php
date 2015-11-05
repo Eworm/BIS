8f9fb32cfd4570f4c94800448a9dbdc5b4cf72a3
Wout <wout@woutmager.nl>
2015-11-05 22:34:22 +0100

    ..

--- admin/admin_uitdevaart_verw.php
+++ admin/admin_uitdevaart_verw.php
@@ -49,8 +49,6 @@ if (!mysql_select_db($database, $link)) {
 $udv_id = $_GET['udv_id'];
 $boot_id = $_GET['boot_id'];
 
-echo "<p><strong>Welkom in de admin van BIS</strong> [<a href=\"./admin_inuitdevaart.php?id=$boot_id\">Terug naar in/uit de vaart van deze boot</a>] [<a href='./admin_logout.php'>Uitloggen</a>]</p>";
-
 $result = mysql_query(sprintf('UPDATE uitdevaart SET Verwijderd=1, Einddatum="%s" WHERE ID=%d', $today_db, $udv_id));
 header('Location: admin_inuitdevaart.php?id=' . $boot_id);
 
